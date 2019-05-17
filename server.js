e5253d91359cb25a9c52802e678e3c7076718627
Rodrigo Vargas <rvargas@cooperativa.cl>
2019-05-17 13:40:48 -0400

    agrego endpoint de cliente y tipocliente

--- server.js
+++ server.js
@@ -53,6 +53,10 @@ const marcaRouters = require('./routers/v1/marca-routers');
 const modeloRouters = require('./routers/v1/modelo-routers');
 const asc_ma_coRouters = require('./routers/v1/asc-marca-concesionaria-routers');
 const asc_mo_sucRouters = require('./routers/v1/asc-modelo-sucursal-routers');
+const tipo_clienteRouters = require('./routers/v1/tipo-cliente-routers');
+const clienteRouters = require('./routers/v1/cliente-routers');
+
+
 
 concesionariaRouters.register(server);
 sucursalRouters.register(server);
@@ -60,6 +64,10 @@ marcaRouters.register(server);
 modeloRouters.register(server);
 asc_ma_coRouters.register(server);
 asc_mo_sucRouters.register(server);
+tipo_clienteRouters.register(server);
+clienteRouters.register(server);
+
+
 
 const cors = corsMiddleware({
   preflightMaxAge: 5,
