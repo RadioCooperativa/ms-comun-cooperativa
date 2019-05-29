2c34f9a3af1f909bae6283c43c3efe84c922f030
Rodrigo Vargas <rodrigovargassanhueza2@gmail.com>
2019-05-29 02:56:14 -0400

    ajuste validador marca

--- src/validators/marca/insert.js
+++ src/validators/marca/insert.js
@@ -2,7 +2,6 @@
 const joi = require('joi');
 
 module.exports = joi.object().keys({
-	id_marca: joi.number().integer().required(),
     nombre_marca:joi.string().max(50).required(),
 	descripcion_marca:joi.string().max(100).default(null).allow(null)
 });
